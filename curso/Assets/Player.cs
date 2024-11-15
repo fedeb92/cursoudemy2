da7c49ad3148a331261fed905e34e19a5e1ef01a
fedeb92 <fedebrown9@gmail.com>
2024-11-15 14:19:20 -0300

    jump and collision detect
    salto y colision

--- curso/Assets/Player.cs
+++ curso/Assets/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -6,7 +7,15 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D rb;
     private Animator anim;
+    [Header("movement")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    [Header("collition info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+
+
     private float xInput;
     
     private void Awake()
@@ -22,22 +31,41 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        xInput = Input.GetAxisRaw("Horizontal");
-        
+        HandleInput();
+        HandleCollision();
         HandleMovement();
         HandleAnimation();
-        
-
     }
 
-    private void HandleAnimation()
+    private void HandleInput()
     {
-        anim.SetFloat("xVelocity", rb.linearVelocity.x);
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
+    private void HandleCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
     }
     private void HandleMovement()
     {
         rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
-
-
+    }
+    private void HandleAnimation()
+    {
+        anim.SetFloat("xVelocity", rb.linearVelocity.x);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x,transform.position.y - groundCheckDistance));
     }
 }
