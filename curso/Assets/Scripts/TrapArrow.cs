173ae2d7682ff73031e2823a527785c39a954150
fedeb92 <fedebrown9@gmail.com>
2025-04-01 17:50:19 -0300

    arrow trap part 2
    en un momento hubo problema con la instancia pero se soluciono solo asi que todo bien

--- curso/Assets/Scripts/TrapArrow.cs
+++ curso/Assets/Scripts/TrapArrow.cs
@@ -2,5 +2,47 @@ using UnityEngine;
 
 public class TrapArrow : TrapTrampoline
 {
-   
+    [Header("adittional info")]
+    [SerializeField] private float cooldown;
+    [SerializeField] private bool rotationRight;
+    [SerializeField] private float rotationSpeed = 120;
+    private int direction = -1;
+    [Space]
+    [SerializeField] float scaleUpSpeed = 10;
+    [SerializeField] Vector3 targetScale;
+
+    private void Start()
+    {
+        transform.localScale = new Vector3(.3f, .3f, .3f);
+    }
+
+    private void Update()
+    {
+        HandleScaleUp();
+
+        HandleRotation();
+    }
+
+    private void HandleScaleUp()
+    {
+        if (transform.localScale.x < targetScale.x)
+            transform.localScale = Vector3.Lerp(transform.localScale, targetScale, scaleUpSpeed * Time.deltaTime);
+    }
+
+    private void HandleRotation()
+    {
+        direction = rotationRight ? -1 : 1;
+        transform.Rotate(0, 0, (rotationSpeed * direction) * Time.deltaTime);
+    }
+
+    private void DestroyMe()
+    {
+        GameObject arrowPrefab = GameManager.instance.arrowPrefab;
+        GameManager.instance.CreateObject(arrowPrefab, transform, cooldown);
+
+        Destroy(gameObject);
+
+    } 
+        
+
 }
