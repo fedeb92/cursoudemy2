0fa54f009fd55db51959757980d3510ef2c1f44d
fedeb92 <fedebrown9@gmail.com>
2025-04-08 19:25:28 -0300

    ARREGLADO EL PROBLEMA DEL FALL PLATAFFORM
    estuve con cambio de unity y el error era por q no habia acrtivado el trigger que activaba el codigo asi que ya lo pude arreglar y deje que se quedara con el nuevo unity pero puede ser que haya quedado algun error que no moleste quiero creer

--- curso/Assets/Scripts/TrapFallingPlatafform.cs
+++ curso/Assets/Scripts/TrapFallingPlatafform.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class TrapFallingPlatafform : MonoBehaviour
@@ -12,9 +13,14 @@ public class TrapFallingPlatafform : MonoBehaviour
     [SerializeField] private float travelDistance;
     private Vector3[] wayPoints;
     private int wayPointIndex;
-    public bool canMove;
+    private bool canMove = false;
 
     [Header("platafform fall details")]
+    [SerializeField] private float impactSpeed = 3;
+    [SerializeField] private float impactDuration = .1f;
+    private float impactTimer;
+    private bool impactHappend;
+    [Space]
     [SerializeField] private float fallDelay = .5f;
 
     private void Awake()
@@ -24,11 +30,14 @@ public class TrapFallingPlatafform : MonoBehaviour
         colliders = GetComponents<BoxCollider2D>();
     }
 
-    private void Start()
+    private IEnumerator Start()
     {
         SetupWaypoints();
+        float randomDelay = Random.Range(0, .6f);
+        yield return new WaitForSeconds(randomDelay);
+        canMove = true;
     }
-
+    
     private void SetupWaypoints()
     {
         wayPoints = new Vector3[2];
@@ -41,6 +50,7 @@ public class TrapFallingPlatafform : MonoBehaviour
 
     private void Update()
     {
+        HandleImpact();
         HandleMovement();
     }
 
@@ -59,13 +69,27 @@ public class TrapFallingPlatafform : MonoBehaviour
         }
     }
 
+    private void HandleImpact() 
+    {
+        if (impactTimer < 0)
+            return;
+
+        impactTimer -= Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, transform.position + (Vector3.down * 10),impactSpeed * Time.deltaTime); 
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (impactHappend)
+            return;
+
         Player player =  collision.gameObject.GetComponent<Player>();
 
         if (player != null) 
         {
             Invoke(nameof(SwitchOffPlatafform), fallDelay);
+            impactTimer = impactDuration;
+            impactHappend = true;
         }
     }
 
@@ -73,9 +97,11 @@ public class TrapFallingPlatafform : MonoBehaviour
     {
         anim.SetTrigger("deactivate");
 
-        //rb.isKinematic = true;
+        canMove = false;
+
+        rb.bodyType = RigidbodyType2D.Dynamic;
         rb.gravityScale = 3.5f;
-        //rb.drag = .5f;
+        rb.linearDamping = .5f;
 
         foreach (BoxCollider2D collider in colliders)
         {
